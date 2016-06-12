c361e9721edfbc0708c61cfe17912b80f4141a3a
Kulkarni, Alok : RBB COO <Alok.Kulkarni@barclayscorp.com>
2016-06-12 18:02:02 +0530

    Initial Commit

---
+++ graphApplication/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  graphApplication
+//
+//  Created by Alok Kulkarni on 12/06/16.
+//  Copyright © 2016 Private. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ graphApplication/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  graphApplication
+//
+//  Created by Alok Kulkarni on 12/06/16.
+//  Copyright © 2016 Private. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
